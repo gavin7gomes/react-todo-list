e0037f377bb7f0e532a4c8568e300b35ef60f450
Gavin Gomes <56307815+gavin7gomes@users.noreply.github.com>
2020-05-01 16:05:35 +0530

    first commit

---
+++ src/components/TodoList.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Item from "./TodoItem";
+
+export default class TodoList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>heelo from TodoList</h1>
+      </div>
+    );
+  }
+}
